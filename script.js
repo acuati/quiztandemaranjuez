1d1f6ff01d17426819acf059747ac2a4f0597d8e
acuati <benalibenaliabdelkbir@gmail.com>
2024-04-12 10:42:23 +0200

    actualizar cambios

---
+++ script.js
@@ -0,0 +1,16 @@
+const audio = document.getElementById('audio');
+const playButton = document.getElementById('play');
+const pauseButton = document.getElementById('pause');
+const stopButton = document.getElementById('stop');
+
+playButton.addEventListener('click', () => {
+    audio.play();
+});
+
+pauseButton.addEventListener('click', () => {
+    audio.pause();
+});
+
+stopButton.addEventListener('click', () => {
+    audio.stop();
+});
